c30cc4b8a4d2ad5f5afdd85107beb11e249411fd
TNT <pxmrtai97@gmail.com>
2020-12-04 19:51:47 +0700

    commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
+import classNames from "classnames";
+import PropTypes from 'prop-types'; // ES6
 
-const todos = ["Go to market", "Buy food", "Make dinner"];
+import "./TodoItem.css";
+import checkImg from "../img/check.svg";
+import unCheckImg from "../img/uncheck.svg";
 
 class TodoItem extends Component {
   render() {
+    const { item, onClick } = this.props;
+    let url = unCheckImg;
+    if (item.isComplete) {
+      url = checkImg;
+    }
+
     return (
-      <div className="TodoItem">
-        <ul>
-         <li>Go to market</li>
-         <li>Buy food</li>
-         <li>Make dinner</li>
-        </ul>
+      <div
+        className={classNames("TodoItem", {
+          "TodoItem-complete": item.isComplete
+        })}
+        
+      > 
+        <img onClick={onClick} alt='nothing' src={url} width={32} height={32} />
+        <p>{this.props.item.title}</p>
       </div>
     );
   }
 }
+
+TodoItem.propTypes={
+  item: PropTypes.shape({
+    isComplete: PropTypes.bool.isRequired,
+    title: PropTypes.string
+  }),
+  onClick: PropTypes.func
+}
+
 export default TodoItem;
